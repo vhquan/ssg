03590ef8c326696a368368eff756cf5daec41287
quanvu20 <quan.vu@brtchip.com>
2025-03-20 15:33:16 +0700

    Add logs when running server

--- src/server.rs
+++ src/server.rs
@@ -2,15 +2,27 @@ use std::fs::File;
 use std::io::Read;
 // use std::path::Path;
 use tiny_http::{Server, Response};
+use chrono::Local;
 
 use crate::utils::get_content_type;
 
 pub fn start_server() {
     let server = Server::http("0.0.0.0:8080").unwrap();
     println!("Server running on port 8080 (accessible from any IP)");
+    println!("Server started at {}", Local::now().format("%Y-%m-%d %H:%M:%S"));
+    println!("Press Ctrl+C to stop the server");
 
     for request in server.incoming_requests() {
         let url = request.url();
+        let method = request.method();
+        let remote_addr = request.remote_addr();
+        println!("[{}] {} {} from {}", 
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            method,
+            url,
+            remote_addr.map_or("unknown".to_string(), |addr| addr.to_string())
+        );
+
         let path = if url == "/" { "static/index.html" } else { &format!("static{}", url) };
 
         if let Ok(mut file) = File::open(path) {
